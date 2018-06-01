328853df3483c173b3b38442760beb5641443a27
aleksey.dobrynin <aleksey.dobrynin@jetbrains.com>
2018-06-01 11:21:51 +0700

    default test results dir

--- src/test/java/one/trifle/JUnitTest.java
+++ src/test/java/one/trifle/JUnitTest.java
@@ -1,11 +1,13 @@
 package one.trifle;
 
+import org.junit.Assert;
 import org.junit.Test;
 
 public class JUnitTest {
     @Test
     public void first(){
         System.out.println("junit 1");
+        Assert.fail("123");
     }
     @Test
     public void second(){

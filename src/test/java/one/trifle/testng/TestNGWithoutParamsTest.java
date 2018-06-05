724e430a22b330eda8f3ef1980e68ab4490ca65c
aleksey.dobrynin <aleksey.dobrynin@jetbrains.com>
2018-06-05 16:20:33 +0700

    add err message

--- src/test/java/one/trifle/testng/TestNGWithoutParamsTest.java
+++ src/test/java/one/trifle/testng/TestNGWithoutParamsTest.java
@@ -6,11 +6,11 @@ import org.testng.annotations.Test;
 public class TestNGWithoutParamsTest {
     @Test
     public void first() {
-        System.out.println("testng 2.1");
+        System.err.println("testng 2.1");
     }
 
     @Test
     public void second() {
-        System.out.println("testng 2.2");
+        System.err.println("testng 2.2");
     }
 }

24aecbbbe7ebcdb44409e8bd7b5b98b1eb864af8
aleksey.dobrynin <aleksey.dobrynin@jetbrains.com>
2018-06-22 11:25:35 +0700

    add mixed module

---
+++ mixed/src/test/java/one/trifle/surefire/mixed/JUnitTest.java
@@ -0,0 +1,10 @@
+package one.trifle.surefire.mixed;
+
+import org.junit.Test;
+
+public class JUnitTest {
+    @Test
+    public void single() {
+        System.out.println("mixed junit 1");
+    }
+}
---
+++ mixed/src/test/java/one/trifle/surefire/mixed/TestNGTest.java
@@ -0,0 +1,11 @@
+package one.trifle.surefire.mixed;
+
+
+import org.testng.annotations.Test;
+
+@Test
+public class TestNGTest {
+    public void single() {
+        System.out.println("mixed testng 1");
+    }
+}

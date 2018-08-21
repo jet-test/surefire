353d1afbfddd62a979055741b90463adc4f9cda5
madlexa <lexa@trifle.one>
2018-08-21 14:35:30 +0700

    add maven info

--- junit/src/test/java/one/trifle/surefire/junit/JUnitSecondTest.java
+++ junit/src/test/java/one/trifle/surefire/junit/JUnitSecondTest.java
@@ -1,13 +1,12 @@
 package one.trifle.surefire.junit;
 
-import org.junit.Assert;
 import org.junit.Test;
 
 public class JUnitSecondTest {
     @Test
     public void first() {
         System.out.println("junit 2.1");
-        Assert.fail("123");
+//        Assert.fail("123");
     }
 
     @Test

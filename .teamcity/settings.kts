50324c9cc543c8c6ed7b1afc94ceba9b66845c5c
Aleksey Dobrynin <aleksey.dobrynin@jetbrains.com>
2018-12-13 11:36:34 +0700

    add kotlin dsl

---
+++ .teamcity/settings.kts
@@ -0,0 +1,78 @@
+import jetbrains.buildServer.configs.kotlin.v2018_2.*
+import jetbrains.buildServer.configs.kotlin.v2018_2.buildFeatures.xmlReport
+import jetbrains.buildServer.configs.kotlin.v2018_2.buildSteps.maven
+import jetbrains.buildServer.configs.kotlin.v2018_2.vcs.GitVcsRoot
+
+/*
+The settings script is an entry point for defining a TeamCity
+project hierarchy. The script should contain a single call to the
+project() function with a Project instance or an init function as
+an argument.
+
+VcsRoots, BuildTypes, Templates, and subprojects can be
+registered inside the project using the vcsRoot(), buildType(),
+template(), and subProject() methods respectively.
+
+To debug settings scripts in command-line, run the
+
+    mvnDebug org.jetbrains.teamcity:teamcity-configs-maven-plugin:generate
+
+command and attach your debugger to the port 8000.
+
+To debug in IntelliJ Idea, open the 'Maven Projects' tool window (View
+-> Tool Windows -> Maven Projects), find the generate task node
+(Plugins -> teamcity-configs -> teamcity-configs:generate), the
+'Debug' option is available in the context menu for the task.
+*/
+
+version = "2018.2"
+
+project {
+
+    vcsRoot(HttpsGithubComJetTestSurefireGitRefsHeadsMaster)
+
+    buildType(Test)
+}
+
+object Test : BuildType({
+    name = "Test"
+
+    artifactRules = """
+        testng/target/surefire-testng-*.jar
+        mixed/target/surefire-mixed-*.jar
+        junit5/target/surefire-junit5-*.jar
+        junit/target/surefire-junit-*.jar
+    """.trimIndent()
+
+    params {
+        param("teamcity.maven.dontReadPomBeforeBuild", "true")
+    }
+
+    vcs {
+        root(HttpsGithubComJetTestSurefireGitRefsHeadsMaster)
+    }
+
+    steps {
+        maven {
+            goals = "clean test"
+            runnerArgs = "-Dmaven.test.failure.ignore=true"
+            mavenVersion = auto()
+            userSettingsSelection = "settings.xml"
+            userSettingsPath = "/usr/local/Cellar/maven/3.5.4/libexec/conf/settings.xml"
+            useOwnLocalRepo = true
+            isIncremental = true
+        }
+    }
+
+    features {
+        xmlReport {
+            rules = "**/*.xml"
+            param("xmlReportParsing.reportType", "goLang")
+        }
+    }
+})
+
+object HttpsGithubComJetTestSurefireGitRefsHeadsMaster : GitVcsRoot({
+    name = "https://github.com/jet-test/surefire.git#refs/heads/master"
+    url = "https://github.com/jet-test/surefire.git"
+})

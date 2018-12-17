f98e1616df7ae36d38cd7dcb9a4d597ec4716970
Aleksey Dobrynin <aleksey.dobrynin@jetbrains.com>
2018-12-17 21:23:44 +0700

    add sub-sub module

---
+++ sub-sub-module1/src/test/java/one/trifle/surefire/module1_1/Module1_1Test.kt
@@ -0,0 +1,10 @@
+package one.trifle.surefire.module1_1
+
+import org.junit.jupiter.api.Test
+
+class Module1_1Test {
+    @Test
+    fun test() {
+        println("module 1.1 test output")
+    }
+}

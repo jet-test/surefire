b98793e379d228402c4a6042e69e71b64d38ac66
Aleksey Dobrynin <aleksey.dobrynin@jetbrains.com>
2019-01-24 14:31:55 +0700

    add a small test

--- big-fail-test/src/test/java/one/trifle/surefire/bigfailtest/BigFailTest.java
+++ big-fail-test/src/test/java/one/trifle/surefire/bigfailtest/BigFailTest.java
@@ -8,8 +8,15 @@ import java.util.stream.IntStream;
 
 public class BigFailTest {
     @Test
-    public void test() {
+    public void big() {
         String result = IntStream.range(1, 1_000_000).mapToObj(String::valueOf).collect(Collectors.joining());
+        Assert.assertEquals("expected" + result, "actual" + new StringBuilder(result).reverse().toString()
+        );
+    }
+
+    @Test
+    public void small() {
+        String result = IntStream.range(1, 10).mapToObj(String::valueOf).collect(Collectors.joining());
         Assert.assertEquals("expected" + result, "actual" + new StringBuilder(result).reverse().toString()
         );
     }

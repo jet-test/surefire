15de7060798b3a3bfd3f2ab91702b12d4f336c0a
Aleksey Dobrynin <aleksey.dobrynin@jetbrains.com>
2019-04-16 14:45:14 +0700

    remove a big test

--- big-fail-test/src/test/java/one/trifle/surefire/bigfailtest/BigFailTest.java
+++ big-fail-test/src/test/java/one/trifle/surefire/bigfailtest/BigFailTest.java
@@ -7,17 +7,4 @@ import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 public class BigFailTest {
-    @Test
-    public void big() {
-        String result = IntStream.range(1, 1_000_000).mapToObj(String::valueOf).collect(Collectors.joining());
-        Assert.assertEquals("expected" + result, "actual" + new StringBuilder(result).reverse().toString()
-        );
-    }
-
-    @Test
-    public void small() {
-        String result = IntStream.range(1, 10).mapToObj(String::valueOf).collect(Collectors.joining());
-        Assert.assertEquals("expected" + result, "actual" + new StringBuilder(result).reverse().toString()
-        );
-    }
 }

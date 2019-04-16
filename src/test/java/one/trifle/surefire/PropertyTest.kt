03d941c8a30b7bab9100e9d0b80e074c930ebf3e
Aleksey Dobrynin <aleksey.dobrynin@jetbrains.com>
2019-04-16 13:43:28 +0700

    print properties

---
+++ src/test/java/one/trifle/surefire/PropertyTest.kt
@@ -0,0 +1,14 @@
+package one.trifle.surefire
+
+import org.junit.Assert
+import org.junit.Test
+
+class PropertyTest {
+    @Test
+    fun property() {
+        Assert.fail("""
+            teamcity.build.tempDir = ${System.getProperty("teamcity.build.tempDir")}
+            java.io.tmpdir = ${System.getProperty("java.io.tmpdir")}
+        """.trimIndent())
+    }
+}

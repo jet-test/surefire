1a1bccaf7933ee2abc240c3a8b909d7c84750120
Aleksey Dobrynin <aleksey.dobrynin@jetbrains.com>
2019-04-30 10:23:35 +0700

    add import

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -1,4 +1,5 @@
 import jetbrains.buildServer.configs.kotlin.v2018_2.*
+import jetbrains.buildServer.configs.kotlin.v2018_2.buildSteps.MavenBuildStep
 import jetbrains.buildServer.configs.kotlin.v2018_2.buildSteps.maven
 import jetbrains.buildServer.configs.kotlin.v2018_2.vcs.GitVcsRoot
 

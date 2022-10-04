01c3950065fb01818fa554eb6285f6f71a89b93d
jet-test <39553554+jet-test@users.noreply.github.com>
2022-10-04 13:26:03 +0200

    Update JUnitAdditionalTest.java

--- junit/src/test/java/one/trifle/surefire/junit/JUnitAdditionalTest.java
+++ junit/src/test/java/one/trifle/surefire/junit/JUnitAdditionalTest.java
@@ -18,4 +18,10 @@ public class JUnitAdditionalTest {
     public void property() {
         System.out.println(System.getProperty("java.io.tmpdir"));
     }
+    
+    @Test
+    public void additional() {
+        System.out.println("none");
+    }
+
 }

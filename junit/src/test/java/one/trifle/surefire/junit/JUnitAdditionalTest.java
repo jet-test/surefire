192db1164dc75589cc6f534bff8a50ea592af4ef
jet-test <39553554+jet-test@users.noreply.github.com>
2022-10-04 13:21:53 +0200

    Create JUnitAdditionalTest.java

---
+++ junit/src/test/java/one/trifle/surefire/junit/JUnitAdditionalTest.java
@@ -0,0 +1,21 @@
+package one.trifle.surefire.junit;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class JUnitAdditionalTest {
+    @Test
+    public void first() {
+        System.out.println("junit 1.1");
+    }
+
+    @Test
+    public void second() {
+        System.out.println("junit 1.2");
+    }
+
+    @Test
+    public void property() {
+        System.out.println(System.getProperty("java.io.tmpdir"));
+    }
+}
